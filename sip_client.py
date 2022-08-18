3acb4699531f7ad57090bab2b1c0e446ebb392f9
Marcel <70788314+msseguin@users.noreply.github.com>
2022-08-17 23:20:46 -0600

    Early testing of Frameworks

---
+++ sip_client.py
@@ -0,0 +1,12 @@
+# import pytwinkle
+
+# # create a new twinkle object
+# caller = Twinkle()
+
+# caller.set_account('msseguin', 'test','Pass1word')
+
+# caller.run()
+
+# print(caller)
+
+## checking out
